355468b563525fc6d3cbc331dfb547acc5b83d03
tmtocb <kittz.esport@gmail.com>
2021-01-02 15:48:07 +0100

    add confirmable in development for devise, updated Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable, :trackable, :confirmable
 
   has_many :courses
 
